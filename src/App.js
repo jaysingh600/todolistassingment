acf923675bf8414542dcc78a118b46c3598f32aa
jaysingh600 <jaykyp40@gmail.com>
2024-06-03 15:27:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './component/TodoList.jsx';
+
+const App = () => {
+    return (
+        <div>
+            <TodoList/>
+        </div>
+    );
+};
+
+export default App;
